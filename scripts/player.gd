17893f5723e42e795302b9a25e809e50a17d3399
aditursynn <91518877+aditursynn@users.noreply.github.com>
2026-03-03 17:25:19 +0100

    added a physics boundary in the main game scene + removed gravity from the player's scripts to enable top-down 2D movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
-
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	# Get input for both axes
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Apply movement or decelerate to 0
+	if direction_x:
+		velocity.x = direction_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
