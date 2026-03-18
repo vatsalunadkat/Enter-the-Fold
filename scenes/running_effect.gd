dcbf7fea5bf01d4552ffbcd4adb4b7260477ca80
agpaneri-98 <agpaneri@gmail.com>
2026-03-18 17:51:11 +0100

    created animations for dryer,baskets,iron, etc. + new assets

--- scenes/running_effect.gd
+++ scenes/running_effect.gd
@@ -1,10 +1,16 @@
 extends Sprite2D
 
-var state = "idle"
+var _tween: Tween
 
 func _ready() -> void:
 	pass
 
-func _process(delta: float) -> void:
-	if state == "running":
-		rotation += delta * 3.0
+func start_register() -> void:
+	_tween = create_tween().set_loops()
+	_tween.tween_property(self, "modulate", Color(1.5, 1.5, 0.5, 1.0), 0.2)
+	_tween.tween_property(self, "modulate", Color.WHITE, 0.2)
+
+func stop_register() -> void:
+	if _tween:
+		_tween.kill()
+	modulate = Color.WHITE
